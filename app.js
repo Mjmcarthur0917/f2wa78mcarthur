99f624c9394ddc5ad77f5e35f5f61f32f6ee878a
Matthew McArthur <mjmcarthur0917@gmail.com>
2022-10-26 18:49:58 -0500

    new endpoint

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var indexRouter = require('./routes/mydata');
+
 
 var app = express();
 
@@ -20,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/myData', myDataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
