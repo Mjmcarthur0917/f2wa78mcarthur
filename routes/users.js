b57f0348d2b1f8fdf74d7e44efd6396b1e85a4d4
Matthew McArthur <mjmcarthur0917@gmail.com>
2022-10-26 19:01:45 -0500

    completed edit users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let times = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times ++;
+  res.send('user accesses are: ', times);
 });
 
 module.exports = router;
